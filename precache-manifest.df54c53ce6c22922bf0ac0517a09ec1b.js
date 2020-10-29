156d6041c93c90fa4588ac3bdc1334369181fbe8
voduytan <tanvd@antsprogrammatic.com>
2020-10-29 10:30:53 +0700

    Updates

---
+++ precache-manifest.df54c53ce6c22922bf0ac0517a09ec1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5f5a32fac426c8a2a177fa4816da36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "818d0808be887069f083",
+    "url": "/static/css/main.fad9f5e2.chunk.css"
+  },
+  {
+    "revision": "f8eba908036af6d75823",
+    "url": "/static/js/2.d28ddc98.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.d28ddc98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "818d0808be887069f083",
+    "url": "/static/js/main.e465f7ec.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
